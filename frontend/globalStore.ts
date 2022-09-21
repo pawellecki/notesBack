f528e2c6eb676dd95779e307150970ffedc334c5
Paweł Łęcki <roreth@wp.pl>
2022-09-21 08:24:28 +0200

    Delete frontend directory

--- frontend/globalStore.ts
+++
@@ -1,8 +0,0 @@
-import { createSignal } from 'solid-js';
-import { NotePreview, LoggedInUser, User } from './globalTypes';
-
-export const [notesPreview, setNotesPreview] = createSignal<NotePreview[]>([]);
-
-export const [loggedInUser, setLoggedInUser] = createSignal<LoggedInUser>();
-
-export const [users, setUsers] = createSignal<User[]>([]);
--- frontend/src/pages/tinder/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { Component } from 'solid-js';
-
-const Tinder: Component = () => {
-  return <div>tinder</div>;
-};
-
-export default Tinder;
